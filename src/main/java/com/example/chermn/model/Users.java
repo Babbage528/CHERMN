dafabee839c59f7793e2914448ff1ecc75dfda2f
Rebecca <r.mullock@connect.qut.edu.au>
2026-04-12 09:05:47 +1000

    Updated users class to include basic fields and properties

--- src/main/java/com/example/chermn/model/Users.java
+++ src/main/java/com/example/chermn/model/Users.java
@@ -1,4 +1,44 @@
 package com.example.chermn.model;
 
 public class Users {
+    // user id is auto-incremented
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String password;
+
+    // Public constructor without id since it is auto-incremented
+    public Users(String firstName, String lastName, String password)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
 }
