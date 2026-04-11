712ec1a8b6b2dc61307aa45d1576efc67ea693d6
Rebecca <r.mullock@connect.qut.edu.au>
2026-04-12 09:03:53 +1000

    Added DatabaseConnection class for DB access

---
+++ src/main/java/com/example/chermn/DatabaseConnection.java
@@ -0,0 +1,27 @@
+package com.example.chermn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static Connection instance = null;
+
+    private DatabaseConnection() {
+        /// NEED TO HAVE THE DATABASE IN THE ROOT DIRECTORY - CHANGE REFERENCE NAME
+        String url = "jdbc:sqlite:database.db";
+        try {
+            instance = DriverManager.getConnection(url);
+        } catch (SQLException sqlEx) {
+            System.err.println(sqlEx);
+        }
+    }
+
+    public static Connection getInstance() {
+        if (instance == null) {
+            new DatabaseConnection();
+        }
+        return instance;
+    }
+}
+
