ae6817fdf7d0c8b38720991b62631aa03668db93
emma-uni <n12019631@qut.edu.au>
2026-04-14 23:53:31 +1000

    Quiz Questions initial UI Design (no button functionality and no pop up yet)

---
+++ src/main/java/com/example/chermn/controller/QuizQuestionsController.java
@@ -0,0 +1,16 @@
+
+package com.example.chermn.controller;
+
+
+import com.example.chermn.QuizQuestions;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class QuizQuestionsController {
+
+}
