47d95e2c02aba4983302c9dfcc592fe625d1b28e
Rebecca <r.mullock@connect.qut.edu.au>
2026-04-15 10:45:05 +1000

    Deleted the generic Hello Application/Controller/View, replacing references with new project specfic files.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.chermn {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.chermn to javafx.fxml;
     exports com.example.chermn;
