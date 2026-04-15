80f5bfa34eb257c25f4cde679741f7eb987286b8
emma-uni <n12019631@qut.edu.au>
2026-04-15 22:37:34 +1000

    Quiz Questions, UI responds to button press, explanation banner added

--- src/main/java/com/example/chermn/controller/QuizQuestionsController.java
+++ src/main/java/com/example/chermn/controller/QuizQuestionsController.java
@@ -7,10 +7,34 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.control.Label;
 
-import java.io.IOException;
+import java.awt.*;
 
 public class QuizQuestionsController {
+    @FXML
+    private Pane container;
 
+    @FXML
+    private Button option1, option2, option3, option4;
+
+    @FXML
+    private Label explanation;
+
+    @FXML
+    private void AnswerSubmitted() {
+
+        explanation.setText("Incorrect / Correct! Strawberries are better to plant in winter.");
+        explanation.setStyle("-fx-background-color: #ECFCE3; -fx-font-size: 20px;");
+
+        option1.setDisable(true);
+        option3.setDisable(true);
+        option4.setDisable(true);
+
+
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.chermn {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens com.example.chermn to javafx.fxml;
     exports com.example.chermn;
