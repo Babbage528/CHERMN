6e8e8008ad24f61b87c1e2bbb0b6f878976070b7
Nik <nikolajasdue@gmail.com>
2026-04-16 12:07:12 +1000

    cleaned up onboarding / removed my test

--- src/main/java/com/example/chermn/OnBoarding.java
+++ src/main/java/com/example/chermn/OnBoarding.java
@@ -13,17 +13,11 @@ public class OnBoarding extends Application{
     public static final int HEIGHT = 720;
     @Override
     public void start(Stage stage) throws Exception {
-        //test below
-        QuestionDAO dao = new QuestionDAO();
-        dao.getQuestionWithAnswers(1);
-
         FXMLLoader fxmlLoader = new FXMLLoader(OnBoarding.class.getResource("onboarding-screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
         stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
-
-        
     }
     public static void main(String[] args) {
         launch();
