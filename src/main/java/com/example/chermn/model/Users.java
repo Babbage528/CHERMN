f01254bf2bdb51aa3bd0aa8ff3798dd880ea6670
Rebecca <r.mullock@connect.qut.edu.au>
2026-05-10 20:42:45 +1000

    Removed redundant else if statement in password validation, and updated username validation condition.

--- src/main/java/com/example/chermn/model/Users.java
+++ src/main/java/com/example/chermn/model/Users.java
@@ -2,7 +2,13 @@ package com.example.chermn.model;
 
 import java.util.Objects;
 
+/**
+ * A model class representing the user in the trivia application
+ * with a username, first name, last name, password, school name,
+ * and an id that is auto-incremented.
+ */
 public class Users {
+
     // user id is auto-incremented
     private int id;
     private String userName;
@@ -12,7 +18,18 @@ public class Users {
     private String schoolName;
 
 
-    // Public constructor for User
+    /**
+     * Constructs a new User with the specified id, username
+     * first name, last name, password, and school name.
+     * Ensures that the inputted values are validated by calling
+     * the setters to construct the User.
+     * @param id the id of the user.
+     * @param userName the username of the user.
+     * @param firstName the first name of the user.
+     * @param lastName the last name of the user.
+     * @param password the password of the user.
+     * @param schoolName the user's school name.
+     */
     public Users(int id, String userName, String firstName, String lastName, String password, String schoolName) {
         this.id = id;
         // calls the setters to ensure validation is upheld when constructing user
@@ -23,7 +40,19 @@ public class Users {
         setSchoolName(schoolName);
     }
 
-    // Public constructor without id since it is auto-incremented
+
+    /**
+     * Constructs a new User with username, first name,
+     * last name, password, and school name.
+     * Leaves out the id since it is auto-incremented.
+     * Ensures that the inputted values are validated by calling
+     * the setters to construct the User.
+     * @param userName the username of the user.
+     * @param firstName the first name of the user.
+     * @param lastName the last name of the user.
+     * @param password the password of the user.
+     * @param schoolName the user's school name.
+     */
     public Users(String userName, String firstName, String lastName, String password, String schoolName)
     {
         // calls the setters to ensure validation is upheld
@@ -35,36 +64,64 @@ public class Users {
     }
 
 
-    public int getid() {
+    /**
+     * Returns for the user's id.
+     * @return the user's id.
+     */
+    public int getId() {
         return id;
     }
 
+    /**
+     * Returns for the user's username.
+     * @return the user's username.
+     */
     public String getUserName() {
 
         return userName;
     }
 
+    /**
+     * Returns for the user's first name.
+     * @return the user's first name.
+     */
     public String getFirstName() {
 
         return firstName;
     }
 
+    /**
+     * Returns for the user's last name.
+     * @return the user's last name.
+     */
     public String getLastName() {
 
         return lastName;
     }
 
+    /**
+     * Returns for the user's password.
+     * @return the user's password.
+     */
     public String getPassword() {
 
         return password;
     }
 
+    /**
+     * Returns for the user's school name.
+     * @return the user's school name.
+     */
     public String getSchoolName()
     {
 
         return schoolName;
     }
 
+    /**
+     * Sets the user's first name after validating the input.
+     * @param firstName the new first name for the user.
+     */
     public void setFirstName(String firstName) {
         // validate the first name, ensuring only letters are entered
         validateName(firstName, "First name");
@@ -72,6 +129,10 @@ public class Users {
         this.firstName = formatName(firstName);
     }
 
+    /**
+     * Sets the user's last name after validating the input.
+     * @param lastName the new last name for the user.
+     */
     public void setLastName(String lastName) {
         // validate the last name, ensuring only letters are entered
         validateName(lastName, "Last name");
@@ -79,19 +140,23 @@ public class Users {
         this.lastName = formatName(lastName);
     }
 
+    /**
+     * Sets the user's password after validating the input.
+     * @param password the new password for the user.
+     */
     public void setPassword(String password)
     {
         if (password == null || password.length() < 5)
         {
             throw new IllegalArgumentException("Password must be a minimum of 5 chars");
         }
-        else if (password.length() < 5)
-        {
-            throw new IllegalArgumentException("Password must be at least 5 characters");
-        }
         this.password = password;
     }
 
+    /**
+     * Sets for the user's school name after validating the input.
+     * @param schoolName the new name for the user.
+     */
     public void setSchoolName(String schoolName)
     {
         validateName(schoolName, "School name");
@@ -99,25 +164,23 @@ public class Users {
         this.schoolName = formatSchoolName(schoolName);
     }
 
-    @Override
-    public String toString() {
-        return "User Information{" +
-                "id=" + id +
-                ", username='" + userName + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-    }
 
-
+    /**
+     * Sets the user's id, which is auto-incremented.
+     * @param autoIncrementedId the new name for the user.
+     */
     public void setId(int autoIncrementedId) {
 
         this.id = autoIncrementedId;
     }
 
 
+    /**
+     * Sets for the user's username after validating the input.
+     * @param username the new username for the user.
+     */
     public void setUsername(String username) {
-        if ((Objects.equals(username, "")) || (username == null))
+        if ( (username == null) || username.trim().isEmpty())
         {
             throw new IllegalArgumentException("Username must not be left blank");
         }
@@ -128,7 +191,13 @@ public class Users {
 
     }
 
-    // a private method to format the name into standard format
+
+    /**
+     * Formats the inputted name into standard format which has the first character
+     * uppercased, with the rest of the name lowercase.
+     * @param name the user's inputted name.
+     * @return the user's formatted name.
+     */
     private String formatName(String name) {
         name = name.trim().toLowerCase();
 
@@ -136,7 +205,13 @@ public class Users {
     }
 
 
-    // validate name method that uses regex
+    /**
+     * Validates the name inputs using regex to follow standard naming conventions,
+     * depending on what name was entered.
+     * Throws an {@link IllegalArgumentException} if validation for the name fails.
+     * @param name the user's inputted name.
+     * @param fieldName the field name to be displayed of the inputted name.
+     */
     private void validateName(String name, String fieldName) {
         if (name == null || name.trim().isEmpty()) {
             throw new IllegalArgumentException(fieldName + " must not be empty.");
@@ -159,8 +234,13 @@ public class Users {
 
     }
 
-    // since school name can have spaces, and hence multiple words
-    // have separte format method
+    /**
+     * Formats the school name inputted to allow multiple words.
+     * Each word is converted to standard name form, with each word starts with
+     * a capital letter, and the rest lowercase.
+     * @param schoolName the user's inputted school name.
+     * @return the formatted school name.
+     */
     private String formatSchoolName(String schoolName) {
 
         // first split the schoolName into the different words stored in the array
@@ -180,6 +260,20 @@ public class Users {
         // trims the final string before returning
         return formattedString.trim();
 
+    }
+
+    /**
+     * Converts the User's details to a string format.     *
+     * @return the user's details in a string sentence.
+     */
+    @Override
+    public String toString() {
+        return "User Information{" +
+                "id=" + id +
+                ", username='" + userName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
     }
 
 }
