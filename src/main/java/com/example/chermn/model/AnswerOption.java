91032c223f301ce442a78f6f32b203c0addd626b
hgrab <n11188413@qut.edu.au>
2026-05-20 14:15:24 +1000

    javadoc for AnswerOption.java

--- src/main/java/com/example/chermn/model/AnswerOption.java
+++ src/main/java/com/example/chermn/model/AnswerOption.java
@@ -7,15 +7,31 @@ public class AnswerOption {
     private String text;
     private boolean isCorrect;
 
+    /**
+     * Creates a new answer option.
+     *
+     * @param text the text displayed for this option
+     * @param isCorrect whether this option is the correct answer
+     */
     public AnswerOption(String text, boolean isCorrect) {
         this.text = text;
         this.isCorrect = isCorrect;
     }
 
+    /**
+     * Returns the text of this answer option.
+     *
+     * @return the option text
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * Returns the text of this answer option.
+     *
+     * @return the option text
+     */
     public boolean isCorrect() {
         return isCorrect;
     }
