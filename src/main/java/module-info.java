77dd6260231b4c703d7d9c28158d6ace4a01f522
Matthew <mbattisson6@gmail.com>
2026-05-20 19:41:49 +1000

    Javadoc comments and tidy for module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,19 +1,33 @@
+/**
+ * Main module descriptor for the Farmer Fred's Trivia application.
+ * Defines the required dependencies and packages for the project.
+ */
 module com.example.chermn {
+    // -- Core UI (JavaFx) --
     requires javafx.controls;
     requires javafx.fxml;
 
+    // -- Other UI Libraries --
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+
+    // -- Database --
     requires org.xerial.sqlitejdbc;
     requires java.sql;
     requires org.json;
+
+    // -- Networking --
     requires java.net.http;
     requires java.desktop;
     requires jbcrypt;
 
+    // -- Reflection access (JavaFX FXML injection) --
     opens com.example.chermn to javafx.fxml;
+    opens com.example.chermn.controller to javafx.fxml;
+
+    // -- Project 'Public' APIs --
     exports com.example.chermn;
     exports com.example.chermn.controller;
     exports com.example.chermn.model;
-    opens com.example.chermn.controller to javafx.fxml;
+
 }
